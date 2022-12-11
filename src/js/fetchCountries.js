be991b677ac790ba1a14bfc7d650c0f6c5c5860a
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-12-11 14:45:23 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,19 @@
 const URL = 'https://restcountries.com/v3.1/name/';
 const fields = 'fields = name,capital,population,flags,languages';
 
+// export function fetchCountries(name) {
+//   return fetch(`${URL}${name}?${fields}`)
+//     .then(response => response.json())
+//     .catch(error => console.log(error));
+// }
+
 export function fetchCountries(name) {
   return fetch(`${URL}${name}?${fields}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
     .catch(error => console.log(error));
 }
